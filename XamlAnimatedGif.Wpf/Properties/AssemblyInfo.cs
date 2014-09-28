592b05e8b13d6c65d6864a018db6af1729dab81b
Thomas Levesque <tom103@free.fr>
2014-09-28 21:51:09 +0200

    Fixed build error in Release

--- XamlAnimatedGif.Wpf/Properties/AssemblyInfo.cs
+++ XamlAnimatedGif.Wpf/Properties/AssemblyInfo.cs
@@ -35,6 +35,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
-#if DEBUG
 [assembly: InternalsVisibleTo("TestApp.Wpf")]
-#endif
