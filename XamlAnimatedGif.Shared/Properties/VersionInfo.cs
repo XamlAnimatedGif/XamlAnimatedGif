e5cb96c0dd7b48362f47550c67f6e2ad10759259
Thomas Levesque <tom103@free.fr>
2015-01-17 04:55:32 +0100

    Bumped version Fixed build script to use MSBuild 14

--- XamlAnimatedGif.Shared/Properties/VersionInfo.cs
+++ XamlAnimatedGif.Shared/Properties/VersionInfo.cs
@@ -3,11 +3,13 @@ using XamlAnimatedGif.Properties;
 
 [assembly: AssemblyVersion(VersionInfo.Version)]
 [assembly: AssemblyFileVersion(VersionInfo.Version)]
+[assembly: AssemblyInformationalVersion(VersionInfo.Version + VersionInfo.PreRelease)]
 
 namespace XamlAnimatedGif.Properties
 {
     class VersionInfo
     {
         public const string Version = "1.0.0.0";
+        public const string PreRelease = "-alpha5";
     }
 }
--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-msbuild XamlAnimatedGif.sln /t:Clean /t:Rebuild /property:Configuration=Release /property:Platform="Any CPU"
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" XamlAnimatedGif.sln /t:Clean /t:Rebuild /property:Configuration=Release /property:Platform="Any CPU"
