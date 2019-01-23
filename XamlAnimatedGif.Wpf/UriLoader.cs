5800ac32e9fbc57f2c64c7110049e5baf5d1097b
Jumar Macato <hikari.netto23@gmail.com>
2019-01-24 00:54:21 +0800

    [SDK] Fix XAG.Wpf csproj.

--- XamlAnimatedGif.Wpf/UriLoader.cs
+++ XamlAnimatedGif.Wpf/UriLoader.cs
@@ -6,9 +6,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-#if !NET40
 using TaskEx = System.Threading.Tasks.Task;
-#endif
 
 namespace XamlAnimatedGif
 {
