f3dbf2bae6bea1d7890010fdcd3779b3bee4810e
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2019-01-30 17:03:55 +0100

    bump version to 1.2.1-beta001

--- XamlAnimatedGif.Shared/Properties/VersionInfo.cs
+++ XamlAnimatedGif.Shared/Properties/VersionInfo.cs
@@ -9,7 +9,7 @@ namespace XamlAnimatedGif.Properties
 {
     internal static class VersionInfo
     {
-        public const string Version = "1.2.0.0";
-        public const string PreRelease = "";
+        public const string Version = "1.2.1.0";
+        public const string PreRelease = "-beta001";
     }
 }
