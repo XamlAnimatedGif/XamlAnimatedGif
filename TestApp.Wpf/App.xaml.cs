1815c158f23d369d58f4a7beba2b8098240768a0
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2020-11-26 00:30:15 +0100

    Add netcore3.1 / net5.0 support
    drop Silverlight, WinRT, WPF 4.0

--- TestApp.Wpf/App.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TestApp.Wpf
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App
-    {
-    }
-}
---
+++ XamlAnimatedGif.Demo/InputBox.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace XamlAnimatedGif.Demo
+{
+    /// <summary>
+    /// Interaction logic for InputBox.xaml
+    /// </summary>
+    public partial class InputBox : Window, INotifyPropertyChanged
+    {
+        public InputBox()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void OKButtonClick(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void CancelButtonClick(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private bool Set<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, newValue))
+                return false;
+
+            field = newValue;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            return true;
+        }
+
+        private string _prompt;
+        public string Prompt
+        {
+            get => _prompt;
+            set => Set(ref _prompt, value);
+        }
+
+        private string _text;
+        public string Text
+        {
+            get => _text;
+            set => Set(ref _text, value);
+        }
+
+        public static string Show(string prompt, string title, string initialValue = null)
+        {
+            var inputBox = new InputBox
+            {
+                Title = title,
+                Prompt = prompt,
+                Text = initialValue
+            };
+
+            if (inputBox.ShowDialog() is true)
+            {
+                return inputBox.Text;
+            }
+
+            return null;
+        }
+    }
+}
